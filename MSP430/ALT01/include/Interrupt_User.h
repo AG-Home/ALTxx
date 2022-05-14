df767b89e7cf3d4aff85575bdb3219acc11253bd
Marco <marco-antonioa@outlook.com>
2022-05-13 20:01:23 -0500

    feat: Add enable/disble interrupts
    related to #11

--- MSP430/ALT01/include/Interrupt_User.h
+++ MSP430/ALT01/include/Interrupt_User.h
@@ -2,8 +2,15 @@
 #define INTERRUPT_USER
 
 #include <msp430g2553.h>
+#include <stdint.h>
 
+extern uint8_t counter;
 void interruptEnable(void);
+
+void interruptDisable(void);
+
+/// Toggle for 0.5sec the pin(s) with a possible error 
+void toggleTimerError(uint8_t u_data);
 
 
 #endif
--- MSP430/ALT01/src/Interrupt_User.c
+++ MSP430/ALT01/src/Interrupt_User.c
@@ -1,6 +1,30 @@
 #include "Interrupt_User.h"
 
+// uint8_t counter = 0;
+uint8_t counter = 0;
+
 void interruptEnable(void)
 {
-    
+    _BIS_SR(GIE);
+    // __enable_interrupt();
+}
+
+void interruptDisable(void)
+{
+    _BIC_SR(GIE);
+    // __disable_interrupt();
+}
+
+void toggleTimerError(uint8_t u_data)
+{
+    P2OUT ^= u_data;
+    counter = 0;
+}
+
+
+//Timer A ISR
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void Timer_A_CCR0_ISR(void)
+{
+    counter++;
 }
