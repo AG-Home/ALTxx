7126545cd7dc301f94737a8e30940698c7b72a5b
MarcoAAG <marco-antonioa@outlook.com>
2022-07-06 17:53:39 -0500

    feat: Add MCU user
    related to #23

---
+++ MSP430/ALT01/include/Mcu_User.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+ * \file    MCU_User.h
+ * \version 1.0
+ * \author  Marco Aguilar
+ * \brief   MCU ECU layer for MSP430G2553
+ ****************************************************************************/
+
+#ifndef MCU_USER_H
+#define MCU_USER_H
+
+#include <Mcu_Types.h>
+
+void MCUU_v_confDCO(t_Freq e_frequence);
+
+void MCUU_v_confBCO2(t_Selm e_selm, t_Divm e_divm, t_Sels e_sels, t_Divs e_divs, t_Dcor e_dcor);
+
+void MCUU_v_confTimerA0(t_Selm e_selm, t_Divm e_divm, t_Sels e_sels, t_Divs e_divs, t_Dcor e_dcor);
+
+#endif
